7d0ca2859955b655c5d9c32e3d68bdadb77b3148
Philip Zingmark <philip.zingmark@gmail.com>
2024-09-25 21:34:05 +0200

    better create venv instructions

--- db/scripts/create-venv.sh
+++ db/scripts/create-venv.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-python -m venv .venv
-.venv/bin/pip install bcrypt
+if [ -d ".venv" ]; then
+    echo ".venv already exists."
+    echo -e "If it doesnt work, try running:\n\tsource .venv/bin/activate\n"
+    echo -e "If you want to deactivate, simply run\n\tdeactivate\n"
+    exit 0
+fi
 
+
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+echo -e "reopen your terminal or run:\n\nsource .venv/bin/activate\n"
