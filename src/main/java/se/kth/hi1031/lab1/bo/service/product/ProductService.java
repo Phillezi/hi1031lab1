d21b416596aa9979702a016d7cf9b3c72732fccc
emilsr <stener99@hotmail.se>
2024-10-04 14:57:41 +0200

    adding docs

--- src/main/java/se/kth/hi1031/lab1/bo/service/product/ProductService.java
+++ src/main/java/se/kth/hi1031/lab1/bo/service/product/ProductService.java
@@ -14,11 +14,30 @@ import se.kth.hi1031.lab1.ui.dto.product.ProductDTO;
  * easier use within the application.</p>
  */
 public class ProductService {
+
+    /**
+     * Retrieves all available products.
+     * 
+     * <p>This method fetches all products from the data source and converts them into a 
+     * list of {@link ProductDTO} for presentation or further processing.</p>
+     * 
+     * @return A list of {@link ProductDTO} representing all available products.
+     */
     public static List<ProductDTO> getProducts() {
         List<ProductDAO> products = ProductDAO.getAllProducts();
         return products.stream().map(ProductDAO::toProduct).map(Product::toDTO).toList();
     }
 
+    /**
+     * Retrieves specific products based on the provided list of IDs.
+     * 
+     * <p>This method fetches products corresponding to the given IDs from the data source. 
+     * It converts the retrieved data into a list of {@link ProductDTO} for presentation or 
+     * further processing.</p>
+     * 
+     * @param ids A list of product IDs to retrieve.
+     * @return A list of {@link ProductDTO} representing the specified products.
+     */
     public static List<ProductDTO> getProducts(List<Integer> ids) {
         List<ProductDAO> products = ProductDAO.getProductsByIds(ids);
         return products.stream().map(ProductDAO::toProduct).map(Product::toDTO).toList();
